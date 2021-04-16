a424e2bbd8b8b00dd1d787612e4bdef3b93961e6
Kacper <you@example.com>
2021-04-16 15:23:50 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '44102e008c636dcd666b7bbedefe07c0'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3177b719-83fe-4688-8ffa-5ca1ef359c59'
+};
