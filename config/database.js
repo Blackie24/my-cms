84d5e83daa88f6f3f9386d9e7a78f13388b59b8f
Kacper <you@example.com>
2021-05-03 15:26:09 +0200

    mongo

--- config/database.js
+++ config/database.js
@@ -4,12 +4,19 @@ module.exports = ({ env }) => ({
     default: {
       connector: "mongoose",
       settings: {
-        host: env("DATABASE_HOST", "cluster0.rcr62.mongodb.net"),
+        //host: env("DATABASE_HOST", "cluster0.rcr62.mongodb.net"),
+        //srv: env.bool("DATABASE_SRV", true),
+        //port: env.int("DATABASE_PORT", 27017),
+        //database: env("DATABASE_NAME", "my-cms"),
+        //username: env("DATABASE_USERNAME", "Admin"),
+        //password: env("DATABASE_PASSWORD", "Kacper_275"),
+
+        host: env("DATABASE_HOST", "mongodb.server495677.nazwa.pl"),
         srv: env.bool("DATABASE_SRV", true),
-        port: env.int("DATABASE_PORT", 27017),
-        database: env("DATABASE_NAME", "my-cms"),
-        username: env("DATABASE_USERNAME", "Admin"),
-        password: env("DATABASE_PASSWORD", "Kacper_275"),
+        port: env.int("DATABASE_PORT", 4019),
+        database: env("DATABASE_NAME", "server495677_masterbee"),
+        username: env("DATABASE_USERNAME", "server495677_masterbee"),
+        password: env("DATABASE_PASSWORD", "Kacper275"),
       },
       options: {
         authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
